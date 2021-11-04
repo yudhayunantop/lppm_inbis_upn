c2b43a3458bceb6037b808f2fab648f21a3a32f7
RivaldoTjio <rivaldohadiwinata@gmail.com>
2021-11-04 21:14:42 +0700

    Update Database Terkini

--- database/ci_blog.sql
+++ database/ci_blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 04, 2021 at 02:17 PM
+-- Generation Time: Nov 04, 2021 at 03:14 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
